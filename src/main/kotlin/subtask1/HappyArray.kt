62dec9eda769d8ae3d9ae33cc50a9b90319fa9a3
Alexey Kuptsov <kuptsovalexey@mail.ru>
2021-05-25 23:36:59 +0600

    feat: implement convertToHappy function

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,18 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        val result = sadArray.toMutableList()
+        var i = 1
+        while (i < (result.size - 1)) {
+            if ((result[i-1] + result[i+1]) < result[i]) {
+                result.removeAt(i)
+                if(i > 1) i--
+            }
+            else {
+                i++
+            }
+        }
+        return result.toIntArray()
     }
 }
